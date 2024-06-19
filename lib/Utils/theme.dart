4981476f083bd7ff7e48579858181f1bb2e12a63
KhuongKayyyyy <zzkhngzz@gmail.com>
2024-06-19 15:20:58 +0700

    first commit

---
+++ lib/Utils/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+final ThemeData appTheme = ThemeData(
+  primaryColor: const Color(0XFF0ACF83),
+  scaffoldBackgroundColor: Colors.white,
+
+);
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+// lib/main.dart
+import 'package:audio_ecommerce_app/Utils/theme.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: appTheme, // Apply the custom theme
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, World!',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Press Me'),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter your name',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
